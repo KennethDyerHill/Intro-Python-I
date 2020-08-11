fa5704847e078f426fe2bcc25dca8f9d4e516054
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2020-08-10 20:47:24 -0400

    Complete

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -1,44 +1,35 @@
 """
-Python tuples are sort of like lists, except they're immutable and
-are usually used to hold heterogenous data, as opposed to lists
-which are typically used to hold homogenous data. Tuples use
-parens instead of square brackets.
-
-More specifically, tuples are faster than lists. If you're looking
-to just define a constant set of values and that set of values
-never needs to be mutated, use a tuple instead of a list.
-
-Additionally, your code will be safer if you opt to "write-protect"
-data that does not need to be changed. Tuples enforce immutability
-automatically.
+Python exposes a terse and intuitive syntax for performing 
+slicing on lists and strings. This makes it easy to reference
+only a portion of a list or string. 
+This Stack Overflow answer provides a brief but thorough
+overview: https://stackoverflow.com/a/509295
+Use Python's slice syntax to achieve the following:
 """
 
-# Example:
+a = [2, 4, 1, 7, 9, 6]
 
-import math
+# Output the second element: 4:
+print(a[1:2])
 
-def dist(a, b):
-    """Compute the distance between two x,y points."""
-    x0, y0 = a  # Destructuring assignment
-    x1, y1 = b
+# Output the second-to-last element: 9
+print(a[-2:-1])
 
-    return math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
+# Output the last three elements in the array: [7, 9, 6]
+print(a[-3:])
 
-a = (2, 7)   # <-- x,y coordinates stored in tuples
-b = (-14, 72)
+# Output the two middle elements in the array: [1, 7]
+print(a[2:4])
 
-# Prints "Distance is 66.94"
-print("Distance is: {:.2f}".format(dist(a, b)))
+# Output every element except the first one: [4, 1, 7, 9, 6]
+print(a[1:])
 
+# Output every element except the last one: [2, 4, 1, 7, 9]
+print(a[:len(a) - 1])
 
+# For string s...
 
-# Write a function `print_tuple` that prints all the values in a tuple
+s = "Hello, world!"
 
-# YOUR CODE HERE
-
-t = (1, 2, 5, 7, 99)
-print_tuple(t)  # Prints 1 2 5 7 99, one per line
-
-# Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
-print_tuple(u)
+# Output just the 8th-12th characters: "world"
+print(s[7:12])
